d0efd7af5e26d2ef48aa4ffa20239c3d69396830
ywang83 <31540979+ywang83@users.noreply.github.com>
2017-09-01 21:36:18 -0500

    Add files via upload

---
+++ shuru.java
@@ -0,0 +1,9 @@
+package hw;
+import java.util.*;
+
+public class shuru {
+    public static void main (String args[]){
+        Scanner a = new Scanner(System.in);
+        System.out.println(a.nextLine());
+    }
+}
