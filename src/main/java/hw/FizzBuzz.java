08a6a212cd695f08e84ea13f2dab82ed5e35d8c5
rogerld <rogerluo1978@gmail.com>
2017-09-03 16:52:07 -0500

    FizzBuzz

---
+++ src/main/java/hw/FizzBuzz.java
@@ -0,0 +1,28 @@
+package hw;
+
+public class FizzBuzz {
+    public String getNumber(int n)
+    {
+        for(n = 1; n<100; n++){
+            if(n%15 == 0 ){
+               System.out.println ("fizzbuzz");
+            }
+            else if(n%5 == 0 ){
+                System.out.println ("buzz");
+            }
+            else if(n%3 == 0 ){
+                System.out.println ("fizz");
+            }
+            else {System.out.println (Integer.toString(n));}
+        }
+        return null;
+    }
+
+
+    public static void main(String arg[]){
+        FizzBuzz fb = new FizzBuzz();
+        fb.getNumber(3);
+    }
+}
+
+
---
+++ src/main/java/hw/callFizzBuzz.java
@@ -0,0 +1,8 @@
+package hw;
+
+public class callFizzBuzz {
+    public static void main(String arg[]){
+        FizzBuzz fb = new FizzBuzz();
+        fb.getNumber(-10);
+    }
+}
