07431ce611fca91bbd196842ecc01aab994ef210
rogerld <ywang83@luc.edu>
2017-09-06 19:35:26 -0500

    FizzBuzz

---
+++ src/main/java/hw/CommandLine.java
@@ -0,0 +1,9 @@
+package hw;
+
+public class CommandLine {
+        public static void main (String[] args) {
+            for (String s: args) {
+                System.out.println(s);
+            }
+        }
+}
