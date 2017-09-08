3e4045e0f31daf30dd9b8ef0c6f3fabd52a81fa4
ywang83 <31540979+ywang83@users.noreply.github.com>
2017-09-08 13:06:05 -0500

    Delete shuru.java

--- shuru.java
+++
@@ -1,9 +0,0 @@
-package hw;
-import java.util.*;
-
-public class shuru {
-    public static void main (String args[]){
-        Scanner a = new Scanner(System.in);
-        System.out.println(a.nextLine());
-    }
-}
