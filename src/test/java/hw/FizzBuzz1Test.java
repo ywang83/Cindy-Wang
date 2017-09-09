fde0aa51da8259dff587f2dfff0048c3b514f07d
rogerld <ywang83@luc.edu>
2017-09-08 20:34:43 -0500

    FizzBuzz

---
+++ src/test/java/hw/FizzBuzz1Test.java
@@ -0,0 +1,13 @@
+package hw;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FizzBuzz1Test {
+    @Test
+    public void FizzBuzz10() {
+        assertEquals("buzz", FizzBuzz1.getNumber(10));    }
+
+
+}
