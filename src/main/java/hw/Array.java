d9c1543afb363101b56a823fb1a083f79821168e
Cindy Wang <ywang83@luc.edu>
2017-09-15 13:13:21 +0000

    improved Array1
    Signed-off-by: Cindy Wang <ywang83@luc.edu>

--- src/main/java/hw/Array.java
+++ src/main/java/hw/Array.java
@@ -1,14 +1,16 @@
-package hw;
+
 
 import java.util.Arrays;
 
 public class Array {
     public static void main (String args[]) {
-        int n[] = new int[100];
-        for (int i=0; i<n.length; i++){
-            n[i]=i+1;
-            //System.out.println(n[i]);
-        }
+        int n[] = new int[10];
+        int i=0;
+        while (n[0]=0; n[i]+=2; i++){
+            //n[i]=0;
+            //n[i]+=2;
+            //System.out.println(n[i])
         System.out.println(Arrays.toString(n));
     }
 }
+ }
